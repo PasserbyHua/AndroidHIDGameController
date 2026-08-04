47f276168314a09216c06ddfa8081736206670f5
huazl <huazl@interthings.com>
2026-08-04 15:36:35 +0800

    释放1.2版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tools.gamecontroller"
         minSdk = 30
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
